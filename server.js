739697a19d3ee4e916d757b4911f400694618342
Shabaz <111643220+SbzAhmd@users.noreply.github.com>
2024-02-11 00:18:01 +0530

    production app
    Full stack portfolio app

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+
+//dotenv config
+dotenv.config();
+
+//rest obj
+const app = express();
+
+// mmiddlewear
+app.use(cors());
+app.use(express.json());
+
+// static files
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// routes
+app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
